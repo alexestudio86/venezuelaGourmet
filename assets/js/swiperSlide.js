df93f02c12b738ed2fd889a305fc1b636e733588
alexestudio86 <80129697+alexestudio86@users.noreply.github.com>
2021-04-14 00:47:46 -0500

    Update swiperSlide.js

--- assets/js/swiperSlide.js
+++ assets/js/swiperSlide.js
@@ -1,13 +1,15 @@
 //SWIPER ANIMATION ONE SLIDE CAROUSEL
 
-var swiper = new Swiper('.swiper-container', {
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+window.onload = () => {
+  var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
